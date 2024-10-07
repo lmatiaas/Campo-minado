c92e4edfb9c60af11af2e64a9d2191752136e36c
Lmatiaas <lucasmatiassouza@gmail.com>
2024-10-07 18:43:03 -0300

    teste

---
+++ src/br/com/lmatiaas/cm/Aplicacao.java
@@ -0,0 +1,12 @@
+package br.com.lmatiaas.cm;
+
+import br.com.lmatiaas.cm.modelo.Tabuleiro;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		System.out.println(tabuleiro);
+		
+	}
+}
